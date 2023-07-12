fc7be73df33db082acaf899e31e28cd30c8f464e
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-07-11 17:23:52 -0700

    JumpModule Pass 2

--- Mech Fighter/Assets/_Scripts/ActionModules/JumpModule.cs
+++ Mech Fighter/Assets/_Scripts/ActionModules/JumpModule.cs
@@ -11,9 +11,8 @@ public class JumpModule : MonoBehaviour
     [SerializeField] private float spherecastOffset;
     [SerializeField] private float jumpHeight;
     public bool IsGrounded { get; private set; } = false;
-    private float groundAngle;
-
-    private Vector3 groundNormal;
+    public float GroundAngle { get; private set; }
+    public Vector3 GroundNormal { get; private set; }
 
     private void Update()
     {
@@ -36,20 +35,20 @@ public class JumpModule : MonoBehaviour
         if (Physics.SphereCast(transform.position, charControlRef.radius, Vector3.down, out RaycastHit hit, charControlRef.height / 2 - charControlRef.radius + spherecastOffset))
         {
             IsGrounded = true;
-            groundAngle = Vector3.Angle(Vector3.up, hit.normal);
-            groundNormal = hit.normal;
+            GroundAngle = Vector3.Angle(Vector3.up, hit.normal);
+            GroundNormal = hit.normal;
 
             if (Physics.BoxCast(transform.position, new Vector3(charControlRef.radius / 2.5f, charControlRef.radius / 3f, charControlRef.radius / 2.5f), Vector3.down, out RaycastHit helpHit, transform.rotation, charControlRef.height / 2 - charControlRef.radius / 2))
             {
-                groundAngle = Vector3.Angle(Vector3.up, helpHit.normal);
+                GroundAngle = Vector3.Angle(Vector3.up, helpHit.normal);
             }
             // Anim.SetBool("Is Jumping", false);
         }
         else
         {
             IsGrounded = false;
-            groundAngle = 0;
-            groundNormal = Vector3.up;
+            GroundAngle = 0;
+            GroundNormal = Vector3.up;
         }
         // Anim.SetBool("Is Grounded", isGrounded);
     }
