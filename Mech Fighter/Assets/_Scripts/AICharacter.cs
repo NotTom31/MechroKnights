3b49fb3cde3270c20328fb6433d2252500bf4211
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2023-07-17 00:09:00 -0700

    good progress on enemy ai
    my brain hurts

--- Mech Fighter/Assets/_Scripts/AICharacter.cs
+++ Mech Fighter/Assets/_Scripts/AICharacter.cs
@@ -2,19 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public enum AIState
+{
+    Idle,
+    Chasing,
+    Melee,
+    Shooting,
+    Jumping,
+    Blocking
+}
 
 public class AICharacter : MonoBehaviour
 {
     [SerializeField] GameObject player;
     private NavMeshAgent navMeshAgent;
 
-    [SerializeField] private JumpModule jumpModuleRef;
-    [SerializeField] private MoveModule moveModuleRef;
-    [SerializeField] private LookModule LookModuleRef;
-    [SerializeField] private MeleeModule meleeModuleRef;
-    [SerializeField] private BlockModule blockModuleRef;
+    public float chaseDistanceThreshold = 20f; //if ai is outside this area, ai should move towards player 100% of the time 
+    public float jumpDistanceThreshold = 5f;
+    public float blockDistanceThreshold = 1.5f;
+    public float meleeDistanceThreshold = 1f;
+    public float shootDistanceThreshold = 10f;
+
+    private JumpModule jumpModuleRef;
+    private MoveModule moveModuleRef;
+    private LookModule lookModuleRef;
+    private MeleeModule meleeModuleRef;
+    private BlockModule blockModuleRef;
     //[SerializeField] private MechState mechStateRef;
-    [SerializeField] private Animator animatorRef;
+    private Animator animatorRef;
 
     private float distToPlayer;
     private Vector2 movement;
@@ -22,47 +39,171 @@ public class AICharacter : MonoBehaviour
     private bool isBlocking;
     private bool isMelee;
     private bool isTakeDamage;
-    private bool isStunned;
+    //private bool isStunned;
+    private float blockDuration;
+
+    private Stack<AIState> stateStack = new Stack<AIState>();
+    private List<AIState> excludedStates = new List<AIState>();
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        jumpModuleRef = GetComponent<JumpModule>();
+        moveModuleRef = GetComponent<MoveModule>();
+        lookModuleRef = GetComponent<LookModule>();
+        meleeModuleRef = GetComponent<MeleeModule>();
+        blockModuleRef = GetComponent<BlockModule>();
+        animatorRef = GetComponent<Animator>();
+
+        stateStack.Push(AIState.Chasing);
+    }
+
+    private float timeSinceLastStateChange;
+    public float stateChangeInterval = 3f; 
 
     private void Update()
     {
         Brain();
     }
 
-    private void Start()
-    {
-        // Initialize the NavMeshAgent component.
-        navMeshAgent = GetComponent<NavMeshAgent>();
-    }
-
     private void Brain()
     {
-        //decide on the action
+        AIState currentState = stateStack.Peek();
+        Debug.Log(currentState);
+
+        if (!isJumping && ShouldChase() && !IsStateExcluded(AIState.Chasing, AIState.Blocking, AIState.Melee, AIState.Shooting))
+        {
+            ClearStateStack();
+            stateStack.Push(AIState.Chasing);
+            return;
+        }
+
+        if (!isJumping)
+        {
+            if (ShouldBlock() && !IsStateExcluded(AIState.Blocking, AIState.Melee, AIState.Shooting))
+            {
+                stateStack.Push(AIState.Blocking);
+            }
+
+            if (ShouldMelee() && !IsStateExcluded(AIState.Melee, AIState.Blocking, AIState.Shooting))
+            {
+                stateStack.Push(AIState.Melee);
+            }
+
+            if (ShouldShoot() && !IsStateExcluded(AIState.Shooting, AIState.Blocking, AIState.Melee))
+            {
+                stateStack.Push(AIState.Shooting);
+            }
+        }
+
+        // Execute the action for the current state.
+        ExecuteStateAction(currentState);
     }
 
-    private void Grab()
+    private void ExecuteStateAction(AIState state)
     {
-        //do grab
-    }
-
-    private void Jump()
-    {
-        //jumpModuleRef.DoJump();
+        switch (state)
+        {
+            case AIState.Chasing:
+                Move();
+                break;
+            case AIState.Jumping:
+                Jump();
+                break;
+            case AIState.Melee:
+                Melee();
+                break;
+            case AIState.Shooting:
+                Shoot();
+                break;
+            case AIState.Blocking:
+                Block(blockDuration);
+                break;
+            case AIState.Idle:
+                break;
+        }
     }
 
     private void Move()
     {
-        //moveModuleRef.
+        movement = player.transform.position - transform.position; //not sure about this
+        moveModuleRef.OnMove(movement);
+    }
+
+    private void Jump()
+    {
+        jumpModuleRef.OnJump();
+        isJumping = true;
     }
 
     private void Melee()
     {
-        //check chance of doing melee 2
+        //shwing
     }
 
-    private void Block(float time)
+    private void Shoot()
     {
-        //blockModuleRef.SetBlock(time);
+        //pew
     }
 
+    private void Block(float blockDuration)
+    {
+        blockModuleRef.OnBlock(blockDuration);
+        isBlocking = true;
+    }
+
+    private bool ShouldChase()
+    {
+        return distToPlayer > chaseDistanceThreshold;
+    }
+
+    private bool ShouldJump()
+    {
+        return distToPlayer < jumpDistanceThreshold;
+    }
+
+    private bool ShouldBlock()
+    {
+        return distToPlayer < blockDistanceThreshold && !isBlocking && !isMelee && !isJumping;
+    }
+
+    private bool ShouldMelee()
+    {
+        return distToPlayer < meleeDistanceThreshold;
+    }
+
+    private bool ShouldShoot()
+    {
+        return distToPlayer < shootDistanceThreshold;
+    }
+
+    private bool IsStateExcluded(params AIState[] states)
+    {
+        foreach (AIState state in states)
+        {
+            if (excludedStates.Contains(state))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ExcludeState(AIState state)
+    {
+        if (!excludedStates.Contains(state))
+        {
+            excludedStates.Add(state);
+        }
+    }
+
+    private void ClearStateStack()
+    {
+        stateStack.Clear();
+    }
+
+    private void ClearExcludedStates()
+    {
+        excludedStates.Clear();
+    }
 }
