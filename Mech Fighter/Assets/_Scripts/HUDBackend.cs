b7fe4ac820ab758fad76b0fc5cdb0323435f381f
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-07-19 20:02:56 -0700

    Enemy Can be Defeated!

--- Mech Fighter/Assets/_Scripts/HUDBackend.cs
+++ Mech Fighter/Assets/_Scripts/HUDBackend.cs
@@ -2,29 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class HUDBackend : MonoBehaviour
 {
     [SerializeField] private Image healthImageRef;
     [SerializeField] private Image energyImageRef;
+    [SerializeField] private MechState mechStateRef;
+    private bool isAIControl = false;
 
     void Awake()
     {
         MechState.OnUIEnergyChange += EnergyHandler;
         MechState.OnUIHPChange += HPHandler;
-
+        if (energyImageRef == null)
+            isAIControl = true;
         // maybe grab max values here instead of getting them every change?
     }
 
     private void HPHandler(float hp, float maxHp, int mechIndex)
     {
-        if (mechIndex != 0)
+        if (mechIndex != mechStateRef.GetMechIndex())
             return;
         healthImageRef.fillAmount = hp / maxHp;
     }
     private void EnergyHandler(float energy, float maxEnergy, int mechIndex)
     {
-        if (mechIndex != 0)
+        if (mechIndex != 0 || isAIControl)
             return;
         energyImageRef.fillAmount = energy / maxEnergy;
     }
