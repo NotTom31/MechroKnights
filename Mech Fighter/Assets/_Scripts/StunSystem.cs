8bac28ab04f7912abbd139a157671196758c6597
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-09-08 22:08:09 -0700

    Stun System Pass 3

--- Mech Fighter/Assets/_Scripts/StunSystem.cs
+++ Mech Fighter/Assets/_Scripts/StunSystem.cs
@@ -10,7 +10,12 @@ public class StunSystem : MonoBehaviour
 
     private const float STUN_THRESHOLD = 100f;
     private float currentStunValue = 0f;
+    private float currentStunnedSeconds = 0f;
+    
     public bool IsStunned { get; private set; } = false;
+
+    public delegate void StunChangeHandler(bool status, int mechIndex, float currentStunVal);
+    public static event StunChangeHandler OnStunChange;
 
     private void Awake()
     {
@@ -20,7 +25,8 @@ public class StunSystem : MonoBehaviour
 
     private void Update()
     {
-        
+        currentStunValue = Mathf.Clamp(currentStunValue - stunDataRef.decayPerSecond, 0.0f, 100f);
+        currentStunnedSeconds -= Time.deltaTime;
     }
 
     private void HandleHit(int mechIndex, bool isBullet)
@@ -30,14 +36,26 @@ public class StunSystem : MonoBehaviour
 
         if (isBullet)
         {
-            currentStunValue += stunDataRef.bulletStun;
+            currentStunValue = Mathf.Clamp(currentStunValue + stunDataRef.bulletStun, 0.0f, 100f);
         }
         else // need more conditions/tests for whether it's a heavy attack
         {
-            currentStunValue += stunDataRef.lightStun;
+            currentStunValue = Mathf.Clamp(currentStunValue + stunDataRef.lightStun, 0.0f, 100f);
         }
+
     }
 
+    private void StunCheck(float currentStun, float currentTime)
+    {
+        if (currentTime <= 0)
+            IsStunned = false;
+        if (currentStun < STUN_THRESHOLD)
+            return;
+
+        IsStunned = true;
+        currentStunnedSeconds = stunDataRef.stunDurationSeconds;
+        OnStunChange?.Invoke(IsStunned, mechStateRef.GetMechIndex(), currentStunValue);
+    }
     public float MoveScale()
     {
         return stunDataRef.movementScale;
