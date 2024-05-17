a5b16b59c24b0af54344bc192658ecacb1db4f8a
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2024-05-17 00:24:08 -0700

    Revert "Merge pull request #4 from NotTom31/stun-system-pass"
    This reverts commit 7153ddbe0d895867aae2180ea444d87cec54874b, reversing
    changes made to 044893abac8b3ce73f7363d09b16bb2b91a094b2.

--- Mech Fighter/Assets/_Scripts/StunSystem.cs
+++ Mech Fighter/Assets/_Scripts/StunSystem.cs
@@ -1,77 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class StunSystem : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private MechState mechStateRef;
-    [SerializeField] private StunData stunDataRef;
-    [SerializeField] private PlayerInput playerInputRef;
-
-    private const float STUN_THRESHOLD = 100f;
-    private float currentStunValue = 0f;
-    private float currentStunnedSeconds = 0f;
-    
+    [SerializeField] public float StunScale { get; private set; } = 0;
     public bool IsStunned { get; private set; } = false;
-
-    public delegate void StunChangeHandler(bool status, int mechIndex, float currentStunVal);
-    public static event StunChangeHandler OnStunChange;
 
     private void Awake()
     {
-        ServiceLocator.ProvideService(this);
-        MechState.OnHitBoxHit += HandleHit;
-    }
-
-    private void Update()
-    {
-        if (!IsStunned)
-            currentStunValue = Mathf.Clamp(currentStunValue - stunDataRef.decayPerSecond, 0.0f, 100f);
-        currentStunnedSeconds -= Time.deltaTime;
-        StunCheck(currentStunValue, currentStunnedSeconds);
-
-    }
-
-    private void HandleHit(int mechIndex, bool isBullet, bool isBlocking)
-    {
-        if (mechIndex != mechStateRef.GetMechIndex() && !isBullet)
-            return;
-        if (isBlocking)
-            return;
-
-        if (isBullet)
-        {
-            currentStunValue = Mathf.Clamp(currentStunValue + stunDataRef.bulletStun, 0.0f, 100f);
-        }
-        else // need more conditions/tests for whether it's a heavy attack
-        {
-            currentStunValue = Mathf.Clamp(currentStunValue + stunDataRef.lightStun, 0.0f, 100f);
-        }
-        Debug.Log($"Mech {mechStateRef.GetMechIndex()} has been hit!\n" +
-                  $"StunValue: {currentStunValue}\n" +
-                  $"Health: {mechStateRef.HP}");
-    }
-
-    private void StunCheck(float currentStun, float currentTime)
-    {
-        if (currentTime <= 0f)
-        {
-            IsStunned = false;
-            playerInputRef.ActivateInput();
-            currentStunValue = 0f;
-        }
-        if (currentStun < STUN_THRESHOLD)
-            return;
-
-        IsStunned = true;
-        playerInputRef.DeactivateInput();
-        currentStunnedSeconds = stunDataRef.stunDurationSeconds;
-        OnStunChange?.Invoke(IsStunned, mechStateRef.GetMechIndex(), currentStunValue);
-    }
-    public float MoveScale()
-    {
-        return stunDataRef.movementScale;
+        GameManager.serviceLocator.ProvideService(this);
     }
 }
